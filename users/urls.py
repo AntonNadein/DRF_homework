fa9d3c9653ff24d02efba9c87b9970b86c061315
Anton <fynjy-666@mail.ru>
2025-02-03 14:35:13 +0300

    add API stripe for payment, apps drf_yasg.

--- users/urls.py
+++ users/urls.py
@@ -2,14 +2,15 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import PaymentListAPIView, UsersCreateAPIview, UsersDetailAPIView
+from users.views import PaymentCreateAPIview, PaymentListAPIView, UsersCreateAPIview, UsersDetailAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path("payment/", PaymentListAPIView.as_view(), name="payment_list"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", UsersCreateAPIview.as_view(), name="register"),
     path("<int:pk>/", UsersDetailAPIView.as_view(), name="user_get"),
+    path("payment/create/", PaymentCreateAPIview.as_view(), name="payment_create"),
+    path("payment/", PaymentListAPIView.as_view(), name="payment_list"),
 ]
